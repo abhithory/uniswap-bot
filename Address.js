62e4f5df00c905c5829d3a267b64747398033325
abhithory <abhithory@gmail.com>
2023-07-25 23:12:47 +0530

    swap script added

--- Address.js
+++ Address.js
@@ -1,2 +1,2 @@
-export const UniswapRouterContract = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+export const UNISWAP_ROUTER_ADDRESS_GOERLI = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 // https://goerli.etherscan.io/address/0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
