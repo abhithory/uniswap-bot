f6394eee82861dfa5fc798f3a440773eb3b4f0cd
abhithory <abhithory@gmail.com>
2023-07-26 21:43:45 +0530

    added updated file

--- updatedata.js
+++ updatedata.js
@@ -2,17 +2,51 @@ var XLSX = require("xlsx");
 const { ethers } = require("ethers")
 const TOKEN_ABI = require("./abi/token.json");
 const { RPC_URL, UNISWAP_ROUTER_ADDRESS, TOKEN_ADDRESS, TOKEN_DECIMALS } = require("./src/config");
+let provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+const TOKEN_CONTRACT = new ethers.Contract(TOKEN_ADDRESS, TOKEN_ABI.abi, provider);
 
-const init = async () => {
-    const AccountDetails = readDataFromFile();
-    for (let i = 0; i < AccountDetails.length; i++) {
-        const publickey = AccountDetails[i].publickey;
+function weiToEth(wei) {
+    const etherValue = ethers.utils.formatEther(String(wei));
+    return etherValue;
+  }
+  
+  // ETH to Wei conversion
+  function ethToWei(eth) {
+    const weiValue = ethers.utils.parseEther(String(eth));
+    return weiValue;
+  }
 
+function updateFile(data) {
+    const ws = XLSX.utils.json_to_sheet(data)
+    const wb = XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(wb, ws, 'Accounts')
+    XLSX.writeFile(wb, 'file/xlfile2.xlsx')
+
+}
+
+function readDataFromFile() {
+    var workbook = XLSX.readFile("file/xlfile.xlsx");
+    var sheet_name_list = workbook.SheetNames;
+    return XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+}
+
+
+const updateBalanceInFile = async () => {
+    const Data = readDataFromFile();
+    console.log("updating data....");
+    for (let i = 0; i < Data.length; i++) {
+        const publickey = Data[i].publickey;
         let tokenTotalBalance = await TOKEN_CONTRACT.balanceOf(publickey);
-        tokenTotalBalance = Number(weiToEth(tokenTotalBalance))
+        let symbol = await TOKEN_CONTRACT.symbol();
+        tokenTotalBalance = String(weiToEth(tokenTotalBalance))
+        Data[i][symbol] = tokenTotalBalance;
     }
+    console.log(Data);
+    // updateFile(Data);
 };
 
 
 
-init()
+updateBalanceInFile()
+
+module.exports = {updateBalanceInFile };
